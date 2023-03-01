b6daa3013aa135de3367876f1225b69d8b194894
Gelagurt <118499299+Gelagurt@users.noreply.github.com>
2023-03-01 18:06:19 -0500

    Cambios

---
+++ app/src/main/java/com/pruebas/Activity_Registro.kt
@@ -0,0 +1,11 @@
+package com.pruebas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_Registro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
