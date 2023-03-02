305da1d051af3f05cf87976b9e5ae06b0cb2b7aa
Haroldp0810 <harold.p2320@gmail.com>
2023-03-01 20:07:57 -0500

    Config reciclevew

---
+++ app/src/main/java/com/pruebas/models/Person.kt
@@ -0,0 +1,10 @@
+package com.pruebas.models
+
+data class Person(
+    //
+    var nombre: String,
+    var apellido: String,
+    var edad: Int,
+    var carrera: String,
+    var correo: String
+)
---
+++ app/src/main/java/com/pruebas/reciclerView_perfiles.kt
@@ -0,0 +1,11 @@
+package com.pruebas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class reciclerView_perfiles : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recicler_view_perfiles)
+    }
+}
