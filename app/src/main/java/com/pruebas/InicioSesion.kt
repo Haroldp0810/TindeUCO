a150a4a94ad067dd6ad2b7bef480cfb4d3e651bf
cristianhurtado7124 <cristian.hurtado7124@soyuco.edu.co>
2023-03-14 21:27:07 -0500

    modificación comunic activity

--- app/src/main/java/com/pruebas/InicioSesion.kt
+++ app/src/main/java/com/pruebas/InicioSesion.kt
@@ -1,11 +1,15 @@
 package com.pruebas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class InicioSesion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_sesion)
+
     }
+
 }
--- app/src/main/java/com/pruebas/MainActivity.kt
+++ app/src/main/java/com/pruebas/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.pruebas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +15,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         variablesyConstantes()
+        val btn = findViewById<Button>(R.id.button3)
+        btn.setOnClickListener {
+            val lanzarInicioSesion=Intent(this, InicioSesion::class.java)
+            startActivity(lanzarInicioSesion)
+        }
     }
 
     private fun variablesyConstantes(){
