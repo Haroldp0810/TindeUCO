4ca402ae0a61bd48f31d80a3feaf807e0c9c06f6
Gelagurt <118499299+Gelagurt@users.noreply.github.com>
2023-03-20 12:24:38 -0500

    RegistroPag2 y presentacion de perfil

---
+++ app/src/main/java/com/pruebas/Mis_Datos.kt
@@ -0,0 +1,11 @@
+package com.pruebas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mis_Datos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mis_datos)
+    }
+}
